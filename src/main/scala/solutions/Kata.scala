d92581d1f22ea8df69da3abe3865f90817e519ed
lhohan <github@lhoest.eu>
2014-02-15 16:31:52 +0100

    Added kata: minor adjustment of solution

--- src/main/scala/solutions/Kata.scala
+++ src/main/scala/solutions/Kata.scala
@@ -151,14 +151,12 @@ object Kata {
    * scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
    * res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
    */
-  def pack[A](ls: List[A]): List[List[A]] = {
-    if (ls.isEmpty) List(List())
+  def pack[A](list: List[A]): List[List[A]] = {
+    if (list.isEmpty) List(List())
     else {
-      val (packed, next) = ls span {
-        _ == ls.head
-      }
-      if (next == Nil) List(packed)
-      else packed :: pack(next)
+      val (current, next) = list.span(_ == list.head)
+      if (next == Nil) List(current)
+      else current :: pack(next)
     }
   }
 
