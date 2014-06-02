616aa717debb5bbde09508dc04653814e3275e07
lhohan <github@lhoest.eu>
2014-06-02 19:51:58 +0200

    sum and product applied with function

--- kata02/src/test/scala/KataTest.scala
+++ kata02/src/test/scala/KataTest.scala
@@ -4,7 +4,8 @@ import org.scalatest.FunSuite
 
 class KataTest extends FunSuite with Timeouts {
 
-//    import solutions.Kata._
+  //    import solutions.Kata._
+
   import kata.Kata._
 
   test("and - default cases") {
@@ -23,6 +24,69 @@ class KataTest extends FunSuite with Timeouts {
     import org.scalatest.time.SpanSugar._
     failAfter(30 millis) {
       and(false, methodTakingTooLongSoWeCanDetectNoPassByName)
+    }
+  }
+
+  def cube = (x: Int) => x * x
+
+  /**
+   * Implement the sum of the numbers between a and b (a smaller than b) with the function passed applied to each number.
+   **/
+  def sum(f: Int => Int)(a: Int, b: Int): Int = a match {
+    case a if a > b => 0
+    case _ => f(a) + sum(f)(a + 1, b)
+  }
+
+  def sum2(f: Int => Int): (Int, Int) => Int = {
+    def sumF(a: Int, b: Int): Int = if (a > b) 0 else f(a) + sumF(a + 1, b)
+    sumF
+  }
+
+
+  /**
+   * Implement the product of the numbers between a and b (a smaller than b) with the function passed applied to each number.
+   **/
+  def product(f: Int => Int)(a: Int, b: Int): Int = a match {
+    case a if a > b => 1
+    case _ => f(a) * product(f)(a + 1, b)
+  }
+
+  /**
+   * Write factorial in terms of product.
+   **/
+  def factorial(n: Int): Int = product(x => x)(1, n)
+
+  /** Write a more general function which generalizes both sum and product */
+  def mapReduce(identity: Int, combine: (Int, Int) => Int)(f: Int => Int)(a: Int, b: Int): Int = a match {
+    case a if a > b => identity
+    case _ => combine(f(a), mapReduce(identity, combine)(f)(a + 1, b))
+  }
+
+  test("sum") {
+    assertResult(14) {
+      sum(cube)(1, 3)
+    }
+  }
+
+  test("sum2") {
+    assertResult(14) {
+      sum2(cube)(1, 3)
+    }
+  }
+
+  test("product") {
+    assertResult(36) {
+      product(cube)(1, 3)
+    }
+  }
+
+  test("factorial") {
+    assertResult(120)(factorial(5))
+  }
+
+  test("product generalized") {
+    assertResult(36) {
+      mapReduce(1, (x, y) => x * y)(cube)(1, 3)
     }
   }
 
